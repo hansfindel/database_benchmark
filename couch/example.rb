36937bd0e5f8d906d52b3852422c4321c9aa12fe
Hans Findel <hanstrax@gmail.com>
2014-06-26 12:04:53 -0400

    Add basic couch example

---
+++ couch/example.rb
@@ -0,0 +1,34 @@
+# run instructions: ruby example.rb
+
+# require 'rubygems'  # not necessary for Ruby 1.9
+# require 'couchrest'
+
+require "./couch.rb" 
+
+server = Couch::Server.new("localhost", "5984")
+
+# server.delete("/foo")
+
+# create a database 
+# db = server.get("/foo")
+# puts db.body
+# server.put("/foo/", "")
+
+
+doc = <<-JSON
+{"type":"comment #{Time.now.to_i}","body":"First Post!"}
+JSON
+document_id = "document_#{Time.now.to_i}"
+server.put("/foo/#{document_id}", doc)
+
+puts "##################################"
+
+res = server.get("/foo/#{document_id}")
+json = res.body
+puts json
+
+
+puts "##################################"
+
+db = server.get("/foo/")
+puts db.body
