5d721058950db2ecb8acf6c2d07582580df2218f
Hans Findel <hanstrax@gmail.com>
2014-06-30 01:17:36 -0400

    Update thread manager implementation

--- _utilities/thread_manager.rb
+++ _utilities/thread_manager.rb
@@ -2,14 +2,25 @@ class ThreadManager
 
     ### run multiple threads without care the implementation 
     ### fun with some metaprogramming and ruby magic
-    ## example: tn.config_for_text(4, RawDataProvider, :web_markup) do |x|;  puts "**#{x.size}**"; end
+    ## example: 
+    # tn = ThreadManager.new; 
+    # tn.config_for_text(4, RawDataProvider, :web_markup) do |x|;  puts "**#{x.size}**"; end
     def config_for_text(number, object_class, target_method)
         list = []
+        _list = []
         number.times do |i|
-            text = object_class.new.send(target_method)
-            list << Thread.new { yield(text) }
+            _list << Thread.new do |t|
+                text = object_class.new.send(target_method)
+                list << Thread.new { yield(text) }
+            end
         end
-        list
+        _list.each do |t|
+            t.join
+        end
+        list.each do |t|
+            t.join
+        end
+        return nil
     end
 
 end
