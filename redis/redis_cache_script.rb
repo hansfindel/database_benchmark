3d597247278896900e8c0aa8df7a0aea05f70cda
Hans Findel <hanstrax@gmail.com>
2014-07-01 00:55:59 -0400

    Redo thread manager to work as in a structured/replicable way

--- redis/redis_cache_script.rb
+++ redis/redis_cache_script.rb
@@ -1,21 +1,21 @@
 require "redis"
-require "../_raw_data/raw_data_provider.rb"
+require "../_raw_data/structured_data_provider.rb"
 require "../_utilities/thread_manager.rb"
 require "../_utilities/test_runner.rb"
 
+# params
+repetitions_per_document = (ARGV[0] || 15).to_i  # number of threads trying to excecute concurrently
 
+# classes required
 redis = Redis.new
 threadManager = ThreadManager.new 
 testRunner = TestRunner.new 
 
-thread_count = 15  # number of threads trying to excecute concurrently
-
-testRunner.run("insert #{thread_count} random files") do 
-
-	threadManager.config_for_text(thread_count, RawDataProvider, :web_markup) do |x|  
-		key_name = "cache_#{Time.now.to_i}-#{Random.rand.to_s[2, 16]}"
-		redis.set(key_name, x)	
+# data required && the script itself
+data_providers = StructuredDataProvider.factory
+testRunner.run("insert #{repetitions_per_document} files") do 
+	threadManager.map(repetitions_per_document, data_providers, :getName, :getHTML) do |name, html|  
+		key_name = "cached_#{name}"
+		redis.set(key_name, html)	
 	end
-
 end
-
