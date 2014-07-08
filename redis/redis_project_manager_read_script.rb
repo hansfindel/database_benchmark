23817eeed2fa15f0e829bc3e4db39619ae05d1d4
Hans Findel <hanstrax@gmail.com>
2014-07-08 11:07:39 -0400

    Add redis reads to the keyvalue-store

---
+++ redis/redis_project_manager_read_script.rb
@@ -0,0 +1,44 @@
+require "redis"
+require "../_utilities/data_loader.rb"
+require "../_utilities/thread_manager.rb"
+require "../_utilities/test_runner.rb"
+require "json"
+
+# params
+magnitude_order = (ARGV[0] || 1).to_i  # number of threads trying to excecute concurrently
+
+# classes required
+redis = Redis.new
+threadManager = ThreadManager.new 
+testRunner = TestRunner.new 
+
+queries = [:users, :projects, :tasks, :columns]
+targets = [:users, :projects, :tasks, :columns]
+
+# data required && the script itself
+data_providers = DataLoader.nested_keyvalue_factory magnitude_order
+testRunner.run("insert #{magnitude_order} files in redis") do |t|
+	threadManager.project_manager_map(data_providers, queries, targets) do |query, query_data|  
+		if query == queries[0]
+			user_key = "#{query.to_s}_#{query_data[:id]}_#{t}"
+			# user = JSON.parse(redis.get(user_key))
+			# proj_ids = user[:project_ids]
+			user = redis.get(user_key).to_s
+			proj_ids = user[user.rindex("project_id").to_i+14..-3].to_s.split(",")
+			proj_ids.each do |p_id|
+				proj_key = "projects_#{p_id}_#{t}"
+				# proj = JSON.parse(redis.get(proj_key))
+				proj = redis.get(proj_key).to_s
+				# puts proj
+				
+				cols_ids = proj[proj.rindex(":columns=>").to_i+9..-3].to_s.split(",")
+				cols_ids.each do |col_id|
+					col_key = "columns_#{col_id}_#{t}"
+					redis.get(col_key)
+				end
+			end
+			# JSON.parse(redis.get("foo"))
+			# puts key_name # tasks_1305_0
+		end
+	end
+end
