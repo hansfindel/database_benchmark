f6befb819ff47953bee0695055e70255f76ae8bd
Hans Findel <hanstrax@gmail.com>
2014-07-07 21:38:29 -0400

    Update data_loader

--- _utilities/data_loader.rb
+++ _utilities/data_loader.rb
@@ -1,5 +1,100 @@
 class DataLoader
 
+    def self.relational_factory(magnitud_order = 1)
+        # for cassandra and for pg
+        self.default_factory(magnitud_order)
+    end
+    def self.relational_document_factory(magnitud_order = 1)
+        data = self.default_factory(magnitud_order)
+        
+        users = []
+        data.users.each do |user_data|
+            users << { id: user_data[0], first_name: user_data[1], last_name: user_data[2], email: user_data[3], password_hash: user_data[4], password_salt: user_data[5], active: user_data[6], deleted: user_data[7] }
+        end
+        projects = []
+        data.projects.each do |project_data|
+            projects << { id: project_data[0], name: project_data[1], description: project_data[2], organization_id: project_data[3], created_by: project_data[4], active: project_data[5] }
+        end
+        project_users = []
+        data.project_users.each do |project_user_data|
+            project_users << { id: project_user_data[0], user_id: project_user_data[1], project_id: project_user_data[2], admin: project_user_data[3] }
+        end
+        tasks = []
+        data.tasks.each do |task_data|
+            tasks << { id: task_data[0], name: task_data[1], description: task_data[2], difficulty: task_data[3], created_by: task_data[4], assigned_to: task_data[5], column_id: task_data[6], completed_at: task_data[7], column_id: task_data[8], priority: task_data[9], seconds_worked: task_data[10] } 
+        end
+        columns = []
+        data.columns.each do |column_data|
+            columns << { id: column_data[0], name: column_data[1], description: column_data[2], color: column_data[3], order: column_data[4], project_id: column_data[5] }
+        end
+        {
+            users: users,
+            projects: projects,
+            project_users: project_users,
+            tasks: tasks,
+            columns: columns
+        }
+    end
+
+    def self.nested_document_factory(magnitud_order = 1)
+        data = self.default_factory(magnitud_order)                
+        users = []
+        data.users.each do |user_data|
+            user = { id: user_data[0], first_name: user_data[1], last_name: user_data[2], email: user_data[3], password_hash: user_data[4], password_salt: user_data[5], active: user_data[6], deleted: user_data[7] }
+            user_projects = []
+            data.projects.each do |project_data|
+                project_users << project_data[2] if project_data[1] == user[:id] # add id to user if has matches with its id
+            end
+            user[:project_ids] = user_projects
+            users << user
+        end
+
+        projects = []
+        data.projects.each do |project_data|
+            project = { name: project_data[0], description: project_data[1], organization_id: project_data[2], created_by: project_data[3], active: project_data[4] }
+            project_users = []
+            data.project_users.each do |project_user_data|
+                project_users << project_data[1] if project_data[2] == project[:id]
+            end    
+            project[:user_ids] = project_users
+            columns = []
+            data.columns.each do |column_data|
+                column = { id: column_data[0], name: column_data[1], description: column_data[2], color: column_data[3], order: column_data[4], project_id: column_data[5] }
+                if column[:project_id] = project[:id]
+                    tasks = []
+                    data.tasks.each do |task_data|
+                        task = { id: task_data[0], name: task_data[1], description: task_data[2], difficulty: task_data[3], created_by: task_data[4], assigned_to: task_data[5], column_id: task_data[6], completed_at: task_data[7], column_id: task_data[8], priority: task_data[9], seconds_worked: task_data[10] } 
+                        tasks << task if task[:column_id] == column[:id]
+                    end
+                    column[:tasks] = tasks
+                    columns << column
+                end
+            end
+            project[:columns] = columns
+            projects << project
+        end
+        
+        { users: users, projects: projects } # return value
+    end
+
+    def self.default_factory(magnitud_order = 1)
+        # default_values = [1000, 200, 1000, 800, 2000] 
+        # default_names  = [:get_users, :get_projects, :get_project_users, :get_columns, :get_tasks]
+        # output = []
+        # 5.times do |i|
+        #     output << self.send(default_names[i], magnitud_order * default_values)
+        # end
+        # output
+        {
+            users: self.get_users(magnitud_order * 1000), 
+            projects: self.get_projects(magnitud_order * 200), 
+            project_users: self.get_project_users(magnitud_order * 1000), 
+            columns: self.get_columns(magnitud_order * 800), 
+            tasks: self.get_tasks(magnitud_order * 2000)
+        }
+    end
+
+    #private (pseudo-private)
     # Users
     def self.get_users(num = 1000)
         arr = []
@@ -22,7 +117,7 @@ class DataLoader
     def self.get_project_users(num = 1000)
         arr = []
         num.times do |i|
-            arr << [i,(i.to_f/5.0).ceil,i%100==0]
+            arr << [i,i,(i.to_f/5.0).ceil,i%100==0]
         end
         return arr.shuffle
     end
